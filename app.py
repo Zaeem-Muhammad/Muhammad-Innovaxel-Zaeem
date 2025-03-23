2a1cd5fdabc5c5f8bedd6580d99fb85fda73ff7d
Zaeem-Muhammad <muhammadzaeem1026@gmail.com>
2025-03-23 21:23:29 +0500

    Add MySQL database connection function

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
+
+
+def create_db_connection():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="password123",
+        database="shortener_db"
+    )
 
 @app.route("/")
 def home():
